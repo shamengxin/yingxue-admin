7edb4abbe71f9413b41467afed4f9cd4edfcf190
shamengxin <1921028746@qq.com>
2023-06-04 23:44:16 +0800

    6.4

---
+++ yingxue-admins/src/main/java/com/shamengxin/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.shamengxin.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("demos")
+public class DemoController {
+
+
+
+    public void demos(){
+        log.info("");
+    }
+
+
+}
